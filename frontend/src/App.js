a3cf25888efdc87a47f625574933776c981403e9
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-04 13:20:05 +0530

    creating header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      hello there
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' collapseOnSelect expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <i className='fas fa-shopping-cart'></i>  Cart</Nav.Link>
+              <Nav.Link href="/login">
+                <i className='fas fa-user'></i> Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
