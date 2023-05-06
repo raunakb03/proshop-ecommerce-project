f4f832b7e861fc8ea127605f32e39f665bbca573
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-06 14:57:47 +0530

    setting up react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,25 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
