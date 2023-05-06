03519c1fb14abde0ffc1d0aefd36042b42efa6e6
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-06 14:40:10 +0530

    create product screen, product componenet and rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from 'react-bootstrap';
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to proshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
