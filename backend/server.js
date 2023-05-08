ecc61d57b1d9b98d1ee7cdc9ad5a990b4eeb0cae
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-08 14:10:32 +0530

    fetching products from database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const app = express();
 import colors from 'colors';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
+
+import productRouter from './routes/productRoute.js';
 
 connectDB();
 
@@ -13,14 +14,7 @@ app.get('/', (req, res) => {
     res.send('api is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRouter);
 
 const PORT = process.env.PORT || 5000;
 
