8e7aef97c0b4db18a126218d0ff27b7f0543ed90
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-10 09:49:44 +0530

    creating token, and getting profile using token

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import { Error } from 'mongoose';
 import User from '../models/userModel.js';
-
+import generatetoken from '../utils/generateToken.js';
 
 
 const authUser = asyncHandler(async (req, res) => {
@@ -15,15 +15,33 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null,
+            token: generatetoken(user._id),
         });
     }
     else {
         res.status(401)
         throw new Error('Invalid Credentials');
     }
-})
+});
+
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        return res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    }
+    else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+});
 
 export {
     authUser,
+    getUserProfile
 }
